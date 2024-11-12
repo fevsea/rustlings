2859dfdd02ff198f1555017f820265e3a6c1f2a6
Alejandro Agustin <aagustinb@gmail.com>
2024-11-12 22:49:55 +0100

    Rustlings done

--- exercises/23_conversions/try_from_into.rs
+++ exercises/23_conversions/try_from_into.rs
@@ -28,14 +28,25 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
 
-    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {}
+    fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        if tuple.0 >= 0 && tuple.0 <= 255 &&
+           tuple.1 >= 0 && tuple.1 <= 255 &&
+           tuple.2 >= 0 && tuple.2 <= 255 {
+            Ok(Self {red: tuple.0 as u8, green: tuple.1 as u8, blue: tuple.2 as u8})
+        } else {
+            Err(IntoColorError::IntConversion)
+        }
+    }
 }
 
 // TODO: Array implementation.
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {}
+    fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        let [red, green, blue] = arr;
+        Self::try_from((red, green, blue))
+    }
 }
 
 // TODO: Slice implementation.
@@ -43,7 +54,14 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
 
-    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {}
+    fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            Err(IntoColorError::BadLen)
+        } else {
+            let fixed_array: Result<[i16; 3], _> = slice.try_into();
+            Self::try_from(fixed_array.unwrap())
+        }
+    }
 }
 
 fn main() {
